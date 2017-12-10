4f1f65e3a5ed1c0a76e9ae5b88459adf9d3af06a
Arthur Julio <arthurjuliorisa@gmail.com>
2017-12-10 19:35:51 +0700

    Tambah Halaman Services

---
+++ application/views/site/v_services.php
@@ -0,0 +1,18 @@
+<!-- service -->
+		<div class="privacy about">
+			<h3>Services</h3>
+			<p class="animi" style="text-align: center;">Temukan diberbagai Sosial Media Kami</p>
+			<div class="agile_about_grids">
+				<div class="col-md-4 agile_about_grid_right">
+					<img src="<?php echo base_url(); ?>assets/images/facebook-logo.png" alt=" " class="img-responsive" />
+				</div>
+				<div class="col-md-4 agile_about_grid_left">
+					<img src="<?php echo base_url(); ?>assets/images/twitter-logo.png" alt=" " class="img-responsive" />
+				</div>
+				<div class="col-md-4 agile_about_grid_left">
+					<img src="<?php echo base_url(); ?>assets/images/instagram-logo.png" alt=" " class="img-responsive" />
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+<!-- //service -->
