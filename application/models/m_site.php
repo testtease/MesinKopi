b130eb3e50ceed4037290df42c158038f562f1e0
mahendrabagoes <34390131+mahendrabagoes@users.noreply.github.com>
2017-12-10 20:48:28 +0700

    Menambah beberapa function
    Menambah function yang disesuaikan yang sebelumnya menggunakan id menjadi slug dan 1 function lagi untuk menampilkan all kategori ke index dan product

--- application/models/m_site.php
+++ application/models/m_site.php
@@ -3,39 +3,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_site extends CI_Model {
 
-	function show_rekomendasi()
-	{
-    	$sql = "SELECT * FROM product WHERE status = 1 ORDER BY id_product DESC";
-        $sql = $this->db->query($sql);
-		return $sql->result();
-    }	
-
-    function get_product_by_id($id)
+    function show_rekomendasi()
     {
-        $sql = "SELECT * FROM product WHERE id_product = '$id'";
+        $sql = "SELECT * FROM product WHERE status = 1 ORDER BY id_product DESC";
+        $sql = $this->db->query($sql);
+        return $sql->result();
+    }   
+
+    function get_product_by_id($slug)
+    {
+        $sql = "SELECT * FROM product WHERE nama_slug = '$slug'";
         $sql = $this->db->query($sql);
         return $sql->row();
     }
 
-    // function show_kategori($kategori)
-    // {
-    //     $sql = "SELECT * FROM product WHERE kategori = '$kategori'";
-    //     $sql = $this->db->query($sql);
-    //     return $sql->row();
-    // }
-
     function show_kategori($kategori)
     {
-        $sql = "SELECT * FROM product, kategori WHERE product.kategori = kategori.id_kategori AND kategori.id_kategori = '$kategori'";
+        $sql = "SELECT * FROM product, kategori WHERE product.kategori = kategori.id_kategori AND kategori.id_kategori = (SELECT id_kategori FROM kategori WHERE product.kategori = kategori.id_kategori AND kategori.kategori_slug = '$kategori')";
         $sql = $this->db->query($sql);
         return $sql->result();
     }
 
     function nama_kategori($kategori)
     {
-        $sql = "SELECT nama_kategori FROM kategori WHERE kategori.id_kategori = '$kategori'";
+        $sql = "SELECT nama_kategori FROM kategori WHERE kategori.kategori_slug = '$kategori'";
         $sql = $this->db->query($sql);
         return $sql->row();
+    }
+
+    function show_all_kategori()
+    {
+        $sql = "SELECT * FROM kategori";
+        $sql = $this->db->query($sql);
+        return $sql->result();
     }
 }
 
